dbe2dbb2c90bf6a48c9ce963f6c26863a3cab07e
Mark Wiltberger <mark.wiltberger@gmail.com>
2023-08-08 14:05:41 -0400

    Added the mongoose settings to the MongoDB in the app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/customer-app', { useNewUrlParser: true }) 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
