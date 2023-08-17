63c04442ee6f2dd921b55862daf487b46f7b579a
Mark Wiltberger <mark.wiltberger@gmail.com>
2023-08-17 10:43:48 -0400

    set up env vars for MongoDB Atlas cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerModule } from './customer/customer.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/customer-app'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DBCONNECTIONSTRING),
     CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// this was the hardcoded MongoDB connection before the env variables.
+//MongooseModule.forRoot('mongodb://127.0.0.1:27017/customer-app'),
