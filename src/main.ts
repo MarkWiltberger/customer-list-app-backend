b36d2832c3fd73a364ccbba4f270c4d72294c9d4
Mark Wiltberger <mark.wiltberger@gmail.com>
2023-08-21 23:47:23 -0400

    Change listen method call in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.enableCors(); // add this line 
-  await app.listen(parseInt(process.env.PORT) || 3000);
+  // await app.listen(parseInt(process.env.PORT) || 3000);
+  // Use this method here:
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
