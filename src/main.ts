172cddf077e3ffd4c318bd34d3b7a2c5854c895f
Mark Wiltberger <mark.wiltberger@gmail.com>
2023-08-26 12:04:36 -0400

    enable CORS chg PORT add dckr samples

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors(); // add this line 
+  app.enableCors(); // add this line 
   // await app.listen(parseInt(process.env.PORT) || 3000);
   // Use this method here:
-  await app.listen(8080);
-  // await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+  // await app.listen(8080);
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
